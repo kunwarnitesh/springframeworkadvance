acc7a841fa2f71676cfa19bde306a2b47f542e5d
nitesh <nitesh.srivastav@tothenew.com>
2017-12-06 17:06:32 +0530

    Use of Model In Web Pages

--- src/main/java/com/spring/demo/service/UserService.java
+++ src/main/java/com/spring/demo/service/UserService.java
@@ -1,4 +1,13 @@
 package com.spring.demo.service;
 
+import com.spring.demo.repository.FormRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    FormRepository formRepository;
+
 }
