488c566701dc085e164fde6da00b61e7f667542b
nitesh <nitesh.srivastav@tothenew.com>
2017-12-19 17:37:49 +0530

    AOP Integrated

--- src/main/java/com/spring/demo/aopdemo/MyAspect.java
+++ src/main/java/com/spring/demo/aopdemo/MyAspect.java
@@ -1,5 +1,8 @@
 package com.spring.demo.aopdemo;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -9,5 +12,18 @@ public class MyAspect {
     @Before("execution(public void callDriver())")
     void beforecallingDriver() {
         System.out.println("Before calling driver");
+    }
+
+    @After("execution(public void callDriver())")
+    void aftercallingDriver() {
+        System.out.println("After calling driver");
+    }
+
+
+    @Around("execution(public void callDriver())")
+    void aroundCallingDriver(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        System.out.println("Around aroundCallingDriver");
+        proceedingJoinPoint.proceed();
+        System.out.println("After aroundCallingDriver");
     }
 }
